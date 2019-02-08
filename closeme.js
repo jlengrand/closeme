80dca3458fe5ddf1ea199449d7910f4261a26d54
Julien Lengrand-Lambert <Julien.Lengrand-Lambert@ing.nl>
2019-02-08 15:14:46 +0100

    Add chrome storage

--- closeme.js
+++ closeme.js
@@ -1,8 +1,25 @@
 const incrementUrl = 'https://us-central1-closeme-1e761.cloudfunctions.net/incrementCounter';
+const storageKey = "popups_closed";
 
 const xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function() {
+xhr.onreadystatechange = () => {
     console.log('Pop-up closed!');
+}
+
+function updateAnalytics(){
+    incrementClosed();
+    xhr.open('GET', incrementUrl, true);
+    xhr.send();
+}
+
+function incrementClosed(){
+    chrome.storage.sync.get([storageKey], (result) => {
+        console.log(result.popups_closed);
+        const value = ((result.popups_closed === undefined) ? 0 : result.popups_closed) + 1;
+        chrome.storage.sync.set({popups_closed : value} , () => {
+            console.log("value is " + value );
+        });
+    });
 }
 
 const observer = new MutationObserver((mutationsList, observer) => {
@@ -10,8 +27,7 @@ const observer = new MutationObserver((mutationsList, observer) => {
     if ( buttons != undefined){
         for (let b of buttons) {
             b.click();
-            xhr.open('GET', incrementUrl, true);
-            xhr.send();
+            updateAnalytics();
         }
     }
 });
