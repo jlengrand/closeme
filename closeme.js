ffdd284a4cef7bfdc6573a822c613139d8342d7c
Julien Lengrand-Lambert <Julien.Lengrand-Lambert@ing.nl>
2019-02-08 14:35:43 +0100

    Add firebase hook

--- closeme.js
+++ closeme.js
@@ -1,11 +1,19 @@
+const incrementUrl = 'https://us-central1-closeme-1e761.cloudfunctions.net/incrementCounter';
+
+const xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function() {
+    console.log('Pop-up closed!');
+}
+
 const observer = new MutationObserver((mutationsList, observer) => {
     const buttons = document.querySelectorAll('.js-dismissMeterButton,.button--close');
     if ( buttons != undefined){
         for (let b of buttons) {
-            b.click(); console.log("closing annoying pop up");
+            b.click();
+            xhr.open('GET', incrementUrl, true);
+            xhr.send();
         }
     }
-    // else console.log("Nothing found");
 });
 
 observer.observe(
